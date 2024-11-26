5e4fb48d47e3677855932d3ea00f5c5a81d7d605
gclgg <107884203@qq.com>
2024-11-26 09:35:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,29 @@
 ip_version_priority = "ipv6"
 
 source_urls = [
-    "http://175.178.251.183:6689/aktvlive.txt",
+    "https://iptv.b2og.com/txt/fmml_ipv6.txt"
     "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
-    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
+    "https://raw.github.com/yuanzl77/IPTV/main/直播/央视频道.txt",
     "http://120.79.4.185/new/mdlive.txt",
-    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
-    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
+     "http://175.178.251.183:6689/aktvlive.txt",
+    "https://raw.github.com/Fairy8o/IPTV/main/PDX-V4.txt",
+    "https://raw.github.com/Fairy8o/IPTV/main/PDX-V6.txt",
     "https://live.zhoujie218.top/tv/iptv6.txt",
     "https://live.zhoujie218.top/tv/iptv4.txt",
     "https://www.mytvsuper.xyz/m3u/Live.m3u",
     "https://tv.youdu.fan:666/live/",
     "http://ww.weidonglong.com/dsj.txt",
     "http://xhztv.top/zbc.txt",
-    "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
-    "https://raw.githubusercontent.com/Guovin/TV/gd/output/result.txt",
+    "https://raw.github.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
+    "https://raw.github.com/Guovin/TV/gd/output/result.txt",
     "http://home.jundie.top:81/Cat/tv/live.txt",
-    "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
+    "https://raw.github.com/vbskycn/iptv/master/tv/hd.txt",
     "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
-    "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
-    "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
+    "https://raw.github.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
+    "https://raw.github.com/PizazzGY/TVBox_warehouse/main/live.txt",
     "https://fm1077.serv00.net/SmartTV.m3u",
-    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
-    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
+    "https://raw.github.com/ssili126/tv/main/itvlist.txt",
+    "https://raw.github.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
 ]
 
 url_blacklist = [
@@ -54,10 +55,8 @@ announcements = [
     {
         "channel": "公告",
         "entries": [
-            {"name": "请阅读", "url": "https://liuliuliu.tv/api/channels/1997/stream", "logo": "http://175.178.251.183:6689/LR.jpg"},
-            {"name": "yuanzl77.github.io", "url": "https://liuliuliu.tv/api/channels/233/stream", "logo": "http://175.178.251.183:6689/LR.jpg"},
-            {"name": "更新日期", "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E4%B8%BB%E8%A7%92.mp4", "logo": "http://175.178.251.183:6689/LR.jpg"},
-            {"name": None, "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E8%B5%B7%E9%A3%8E%E4%BA%86.mp4", "logo": "http://175.178.251.183:6689/LR.jpg"}
+            {"name": "更新日期",
+            {"name": None, 
         ]
     }
 ]
